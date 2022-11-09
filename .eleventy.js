b8a2bc334553615f2672e670393b3371cbe6bcf0
Emmanuel Etti <emmanueletti@gmail.com>
2022-11-08 17:16:50 -0800

    Saving progress

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/assets/'); // have to tell 11ty to copy over specific folders
+  eleventyConfig.addPassthroughCopy('src/css/'); // have to tell 11ty to copy over specific folders
+  eleventyConfig.addWatchTarget('src/css/'); // 11ty only watches certain files for change by default, have to specify the others
 
   return {
     dir: {
